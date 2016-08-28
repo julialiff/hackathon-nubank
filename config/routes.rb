bd77af4b1947d638b5aaff416b9e6d33ce5f131c
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-08-27 22:33:36 -0300

    getting name from user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :messages
+  get 'sessions/new'
+
   resources :ads
   resources :consultants
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
