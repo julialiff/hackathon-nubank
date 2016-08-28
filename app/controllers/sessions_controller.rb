f0397bfd6217592999edd96f6489653c0c3ca163
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-08-28 09:25:28 -0300

    Consultants dashboard"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,6 +23,7 @@ class SessionsController < ApplicationController
       render 'newi'
     end
   end
+
   def destroy
     log_out
     redirect_to root_url
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,8 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  def log_inc(user)
+
+  def log_inc(consultant)
     session[:consultant_id] = consultant.id
   end
 
@@ -13,11 +14,17 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
   def current_consultant
-    @current_consultant ||= Consultant.find_by(id: session[:user_id])
+    @current_consultant ||= Consultant.find_by(id: session[:consultant_id])
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
+    # ap 'session'
+    # ap session
+    # ap 'current_user:'
+    # ap current_user
+    # ap 'current_consultant'
+    # ap current_consultant
     !current_user.nil? || !current_consultant.nil?
   end
 
